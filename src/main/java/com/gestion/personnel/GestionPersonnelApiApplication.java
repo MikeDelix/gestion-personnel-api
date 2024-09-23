b1f64eb2725b6e3fa18e3d5ffee61940cee70e5d
Chancelin <chancelinmakosso@gmail.com>
2024-09-23 18:56:25 +0000

    ok

---
+++ src/main/java/com/gestion/personnel/GestionPersonnelApiApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.personnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPersonnelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPersonnelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion/personnel/GestionPersonnelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.personnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPersonnelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
